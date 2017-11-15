440651626e1acf231232a651bcf2be70feacf12d
adamfauchoux <adam.fauchoux@cuw.edu>
2017-11-15 10:55:32 -0600

    Initial commit

---
+++ app/src/main/java/com/example/adamfauchoux/csc250_hw13/Screen2.java
@@ -0,0 +1,15 @@
+package com.example.adamfauchoux.csc250_hw13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen2 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+    }
+}
